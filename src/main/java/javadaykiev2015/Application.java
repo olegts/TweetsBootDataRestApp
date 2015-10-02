4bd60fe910214d5bdde3bcb0edafa214f0fa96f6
Oleg Tsal-Tsalko <oleg.tsalko@gmail.com>
2015-10-02 16:50:08 +0300

    Add simple Validator example

--- src/main/java/javadaykiev2015/Application.java
+++ src/main/java/javadaykiev2015/Application.java
@@ -3,6 +3,7 @@ package javadaykiev2015;
 import com.mongodb.Mongo;
 import javadaykiev2015.mongo.converter.StringToZonedDateTimeConverter;
 import javadaykiev2015.mongo.converter.ZonedDateTimeToStringConverter;
+import javadaykiev2015.validator.CreateTweetValidator;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -33,6 +34,12 @@ public class Application {
                 new ZonedDateTimeToStringConverter(),
                 new StringToZonedDateTimeConverter()));
     }
+
+    //This doesn't because of existing BUG - https://jira.spring.io/browse/DATAREST-524
+    /*@Bean
+    public CreateTweetValidator beforeCreateTweetValidator() {
+        return new CreateTweetValidator();
+    }*/
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
--- src/main/java/javadaykiev2015/Initializer.java
+++ src/main/java/javadaykiev2015/Initializer.java
@@ -1,8 +1,10 @@
 package javadaykiev2015;
 
 import javadaykiev2015.repository.TweetRepository;
+import javadaykiev2015.validator.CreateTweetValidator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.data.rest.core.event.ValidatingRepositoryEventListener;
 import org.springframework.stereotype.Component;
 
 /**
@@ -14,8 +16,14 @@ public class Initializer implements CommandLineRunner{
     @Autowired
     TweetRepository repository;
 
+    @Autowired
+    ValidatingRepositoryEventListener validatingRepositoryEventListener;
+
     @Override
     public void run(String... strings) throws Exception {
+
+        validatingRepositoryEventListener.addValidator("beforeCreate", new CreateTweetValidator());
+
         /*repository.save(aTweet()
                 .withId("123")
                 .withText("All Blacks won!")
